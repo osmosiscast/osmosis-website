10e5e501d35972fae0149b9acc867d2fc4d6a3c5
Jamie McMillan <jamie.mcmillan@satellitevu.com>
2024-06-27 09:12:06 +0100

    chore: python linting on commit

--- scripts/osmosisDistributeAudio.py
+++ scripts/osmosisDistributeAudio.py
@@ -12,19 +12,21 @@ import yaml
 from dataclasses import dataclass
 from typing import Optional
 
+
 @dataclass
 class ShowNotes:
     """
     With a show notes file, return all attributes needed to build website page
     """
-    metadata: dict
 
+    metadata: dict
 
     @classmethod
     def read_file(self, filename: str) -> ShowNotes:
-
         with tempfile.NamedTemporaryFile() as temporary_input_metadata:
-            os.system(f"sed '1d;/---/,$d' {filename} >| {temporary_input_metadata.name}")
+            os.system(
+                f"sed '1d;/---/,$d' {filename} >| {temporary_input_metadata.name}"
+            )
 
             with open(temporary_input_metadata.name) as stream:
                 try:
@@ -39,31 +41,56 @@ class ShowNotes:
 
         metadata["season"] = int(metadata["season"])
         metadata["number"] = int(metadata["number"])
-        metadata["slug"] = "/posts/" + subprocess.run(["osmosisKebabCase", metadata["title"]], capture_output=True, text=True).stdout.strip()
-        metadata["description"] = subprocess.run(["osmosisExtractDescription", filename], capture_output=True, text=True).stdout.strip()
-        metadata["body"] = subprocess.run(["osmosisExtractBody", filename], capture_output=True, text=True).stdout.strip()
-
-        return self(
-            metadata=metadata
+        metadata["slug"] = (
+            "/posts/"
+            + subprocess.run(
+                ["osmosisKebabCase", metadata["title"]], capture_output=True, text=True
+            ).stdout.strip()
         )
+        metadata["description"] = subprocess.run(
+            ["osmosisExtractDescription", filename], capture_output=True, text=True
+        ).stdout.strip()
+        metadata["body"] = subprocess.run(
+            ["osmosisExtractBody", filename], capture_output=True, text=True
+        ).stdout.strip()
+
+        return self(metadata=metadata)
+
 
 class PrepareAudio:
     """
     Apply validation to audio file and ship to storage
     """
+
     size: int
     duration: int
-    
+
     def __init__(self, source_filename: str) -> None:
         self.source_filename = source_filename
-        self.size = int(subprocess.run(["osmosisRetrieveAudioMetadata", "-s", self.source_filename], capture_output=True, text=True).stdout.strip())
-        self.duration = int(subprocess.run(["osmosisRetrieveAudioMetadata", "-d", self.source_filename], capture_output=True, text=True).stdout.strip())
+        self.size = int(
+            subprocess.run(
+                ["osmosisRetrieveAudioMetadata", "-s", self.source_filename],
+                capture_output=True,
+                text=True,
+            ).stdout.strip()
+        )
+        self.duration = int(
+            subprocess.run(
+                ["osmosisRetrieveAudioMetadata", "-d", self.source_filename],
+                capture_output=True,
+                text=True,
+            ).stdout.strip()
+        )
         return None
 
     def apply_audio_metadata(self, title: str, date: str) -> None:
-        exit_status = os.system(f"osmosisApplyAudioMetadata {self.source_filename} \"{title}\" \"{date}\" ")
+        exit_status = os.system(
+            f'osmosisApplyAudioMetadata {self.source_filename} "{title}" "{date}" '
+        )
         if exit_status != 0:
-            raise ValueError(f"Unable to apply audio metadata to {self.source_filename}")
+            raise ValueError(
+                f"Unable to apply audio metadata to {self.source_filename}"
+            )
 
         return None
 
@@ -73,38 +100,58 @@ class PrepareAudio:
         season_string = f"{season_number:03.0f}"
         episode_string = f"{episode_number:03.0f}"
         local_filepath = os.path.basename(self.source_filename)
-        upload_filepath = os.path.join(bucket, season_string, episode_string, local_filepath)
+        upload_filepath = os.path.join(
+            bucket, season_string, episode_string, local_filepath
+        )
         audio_url = os.path.join(domain, season_string, episode_string, local_filepath)
 
-        exit_status = os.system(f"npx wrangler r2 object put --file {self.source_filename} \"{upload_filepath}\"")
+        exit_status = os.system(
+            f'npx wrangler r2 object put --file {self.source_filename} "{upload_filepath}"'
+        )
         if exit_status == 0:
             return audio_url
         else:
-            raise ValueError(f"{upload_filepath} failed to upload to R2 with this exit code {exit_status}")
+            raise ValueError(
+                f"{upload_filepath} failed to upload to R2 with this exit code {exit_status}"
+            )
+
 
 class PrepareEpisodePage:
     """
     Write a markdown file to a directory with all required gubbins
     """
+
     show_notes: ShowNotes
     audio_filename: str
     output_directory: str
 
-    def __init__(self, show_notes: ShowNotes, audio_filename: str, output_directory: Optional[str]) -> None:
+    def __init__(
+        self,
+        show_notes: ShowNotes,
+        audio_filename: str,
+        output_directory: Optional[str],
+    ) -> None:
         self.show_notes = show_notes
         self.audio_filename = audio_filename
-        
-        if output_directory is None:
 
+        if output_directory is None:
             date = self.show_notes.metadata["date"].replace("-", "")
             season = f"{self.show_notes.metadata['season']:03.0f}"
             episode = f"{self.show_notes.metadata['number']:03.0f}"
-            title = subprocess.run(["osmosisKebabCase", self.show_notes.metadata["title"]], capture_output=True, text=True).stdout.strip()
+            title = subprocess.run(
+                ["osmosisKebabCase", self.show_notes.metadata["title"]],
+                capture_output=True,
+                text=True,
+            ).stdout.strip()
 
-            website_repository_directory = os.path.expanduser("~/Documents/Development/osmosis-website/content/posts")
+            website_repository_directory = os.path.expanduser(
+                "~/Documents/Development/osmosis-website/content/posts"
+            )
             content_directory = f"{date}-{season}-{episode}-{title}"
 
-            self.output_directory = os.path.join(website_repository_directory, content_directory)
+            self.output_directory = os.path.join(
+                website_repository_directory, content_directory
+            )
         else:
             self.output_directory = output_directory
 
@@ -118,7 +165,7 @@ class PrepareEpisodePage:
         """
 
         header_metadata = dict(self.show_notes.metadata)
-        header_metadata.pop('body', None)
+        header_metadata.pop("body", None)
         with tempfile.NamedTemporaryFile() as temporary_output_metadata:
             with open(temporary_output_metadata.name, "a") as stream:
                 try:
@@ -138,6 +185,7 @@ class PrepareEpisodePage:
             destination = shutil.copy(temporary_output_metadata.name, output_filepath)
 
         return destination
+
 
 def argument_parser() -> argparse.ArgumentParser:
     """
@@ -179,15 +227,23 @@ def main() -> None:
 
     show_notes.metadata["size"] = audio.size
     show_notes.metadata["duration"] = audio.duration
-    audio.apply_audio_metadata(show_notes.metadata["title"], show_notes.metadata["date"])
+    audio.apply_audio_metadata(
+        show_notes.metadata["title"], show_notes.metadata["date"]
+    )
 
     if arguments.output_directory is None:
-        audio_filename = audio.write_to_r2(show_notes.metadata["season"], show_notes.metadata["number"])
+        audio_filename = audio.write_to_r2(
+            show_notes.metadata["season"], show_notes.metadata["number"]
+        )
     if arguments.output_directory is not None:
         audio_filename = arguments.source_audio
 
     show_notes.metadata["url"] = audio_filename
-    episode_page = PrepareEpisodePage(show_notes=show_notes, audio_filename=audio_filename, output_directory=arguments.output_directory)
+    episode_page = PrepareEpisodePage(
+        show_notes=show_notes,
+        audio_filename=audio_filename,
+        output_directory=arguments.output_directory,
+    )
     destination = episode_page.write_markdown()
 
     print(f"Episode prepared at\n{destination}")
@@ -197,4 +253,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
--- scripts/osmosisUploadR2.py
+++ scripts/osmosisUploadR2.py
@@ -2,10 +2,10 @@ import boto3
 import os
 
 account_id = os.getenv("CLOUDFLARE_ACCOUNT_ID")
-session = boto3.Session(profile_name='osmosis')
+session = boto3.Session(profile_name="osmosis")
 s3 = session.client(
-    service_name ="s3",
-    endpoint_url = f"https://{account_id}.r2.cloudflarestorage.com",
+    service_name="s3",
+    endpoint_url=f"https://{account_id}.r2.cloudflarestorage.com",
 )
 
 # Get object information
@@ -17,7 +17,7 @@ print(object_information)
 # Upload/Update single file
 filename = "internal/testing/audio.mp3"
 key_name = "content/episodes/001/001/thisIsNotReal.mp3"
-with open(filename, 'rb') as file:
+with open(filename, "rb") as file:
     s3.upload_fileobj(file, bucket_name, key_name)
 
 object_information = s3.head_object(Bucket=bucket_name, Key=key_name)
