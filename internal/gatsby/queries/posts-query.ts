4092d50dfdfcb6282e6686d4ffd0f8427cddc2e0
Jamie McMillan <jamie.mcmillan@satellitevu.com>
2024-08-20 17:39:13 +0100

    chore: reverting post-query back to main

--- internal/gatsby/queries/posts-query.ts
+++ internal/gatsby/queries/posts-query.ts
@@ -8,7 +8,7 @@ export interface PostsQueryResult {
   };
 }
 
-const getAllPosts = async (graphql: CreatePagesArgs["graphql"]) => {
+const postsQuery = async (graphql: CreatePagesArgs["graphql"]) => {
   const result = await graphql<PostsQueryResult>(`
     {
       allMarkdownRemark(
@@ -33,37 +33,5 @@ const getAllPosts = async (graphql: CreatePagesArgs["graphql"]) => {
 
   return result?.data?.allMarkdownRemark;
 };
-
-const getPublishedPosts = async (graphql: CreatePagesArgs["graphql"]) => {
-  const result = await graphql<PostsQueryResult>(`
-    {
-      allMarkdownRemark(
-        filter: {
-          frontmatter: { draft: { ne: true }, template: { eq: "post" } }
-          fields: { isScheduledPost: { eq: false } }
-        }
-      ) {
-        edges {
-          node {
-            frontmatter {
-              template
-              slug
-            }
-            fields {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `);
-
-  return result?.data?.allMarkdownRemark;
-};
-
-const postsQuery = (queryBody) =>
-  process.env.NODE_ENV === "production"
-    ? getPublishedPosts(queryBody)
-    : getAllPosts(queryBody);
 
 export default postsQuery;
