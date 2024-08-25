1b41104f0676ebb0a534035e3fd9252da240f62c
Jamie McMillan <jamie.mcmillan@satellitevu.com>
2024-08-25 21:38:29 +0100

    chore: added scheduler code to the repo

---
+++ scripts/osmosisScheduler.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Build website
+curl -X POST ${OSMOSIS_BUILD_WEBHOOK}
+
+# Notify episode release (discord)
+episode_directory="/volume8/share/"
+temporary_payload_list="/tmp/osmosisScheduler_payload_files.log"
+find ${episode_directory} -name "*.json" -exec grep -l "$(date '+%Y-%m-%d')" {} >| ${temporary_payload_list} \;
+number_of_files=$(cat ${temporary_payload_list} | wc -l)
+if [ "${number_of_files}" -eq "1" ]
+then
+  payload_file="$(cat ${temporary_payload_list})"
+else
+  payload_file="/tmp/osmosisScheduler_payload.json"
+  echo '{"content": "Task scheduler bot"}' >| ${payload_file}
+fi
+
+curl -H "Content-Type: application/json" -X POST -d @${payload_file} ${EPISODE_RELEASE_WEBHOOK}
