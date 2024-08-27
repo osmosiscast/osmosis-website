cc3aad2c6b2a034ecedcc558fb87d784d51f8f52
Jamie McMillan <jamie.mcmillan@satellitevu.com>
2024-08-27 18:09:37 +0100

    chore: add quotation marks

--- scripts/osmosisScheduler.sh
+++ scripts/osmosisScheduler.sh
@@ -6,7 +6,7 @@ curl -X POST ${OSMOSIS_BUILD_WEBHOOK}
 # Notify episode release (discord)
 episode_directory="/volume8/share/"
 temporary_payload_list="/tmp/osmosisScheduler_payload_files.log"
-find ${episode_directory} -name "*.json" -exec grep -l "$(date '+%Y-%m-%d')" {} >| ${temporary_payload_list} \;
+find "${episode_directory}" -name "*.json" -exec grep -l "$(date '+%Y-%m-%d')" {} >| ${temporary_payload_list} \;
 number_of_files=$(cat ${temporary_payload_list} | wc -l)
 if [ "${number_of_files}" -eq "1" ]
 then
