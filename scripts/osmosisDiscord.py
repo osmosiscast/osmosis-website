bd043d9bcd1b69236e604cd0dfb9468c5e9945c0
Jamie McMillan <jamie.mcmillan@satellitevu.com>
2024-09-06 21:49:18 +0100

    chore: sending to pre-release webhook inaddition

--- scripts/osmosisDiscord.py
+++ scripts/osmosisDiscord.py
@@ -1,6 +1,6 @@
 import json
 import os
-from datetime import datetime, timezone
+from datetime import datetime
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
 
@@ -14,28 +14,37 @@ def discord_episode_release(
     video_url: str,
     output_filename: str,
 ) -> dict:
-    webhook = DiscordWebhook(url=str(os.getenv("EPISODE_RELEASE_WEBHOOK")))
+    prerelease_webhook = DiscordWebhook(
+        url=str(os.getenv("EPISODE_PRERELEASE_WEBHOOK"))
+    )
+    release_webhook = DiscordWebhook(url=str(os.getenv("EPISODE_RELEASE_WEBHOOK")))
 
     description_embed = DiscordEmbed(title=title, description=description)
     description_embed.set_timestamp(release_date)
-    webhook.add_embed(description_embed)
+    prerelease_webhook.add_embed(description_embed)
+    release_webhook.add_embed(description_embed)
 
     youtube_embed = DiscordEmbed(title="Live on YouTube")
     youtube_embed.set_url(url=youtube_url)
-    webhook.add_embed(youtube_embed)
+    prerelease_webhook.add_embed(youtube_embed)
+    release_webhook.add_embed(youtube_embed)
 
     website_embed = DiscordEmbed(title="Website post")
     website_embed.set_url(url=website_url)
-    webhook.add_embed(website_embed)
+    prerelease_webhook.add_embed(website_embed)
+    release_webhook.add_embed(website_embed)
 
     audio_embed = DiscordEmbed(title="Direct audio")
     audio_embed.set_url(url=audio_url)
-    webhook.add_embed(audio_embed)
+    prerelease_webhook.add_embed(audio_embed)
+    release_webhook.add_embed(audio_embed)
 
     video_embed = DiscordEmbed(title="Direct video")
     video_embed.set_url(url=video_url)
-    webhook.add_embed(video_embed)
+    prerelease_webhook.add_embed(video_embed)
+    release_webhook.add_embed(video_embed)
 
+    prerelease_webhook.execute()
     with open(output_filename, "w", encoding="utf-8") as f:
-        json.dump(webhook.json, f)
-    return webhook.json
+        json.dump(release_webhook.json, f)
+    return release_webhook.json
