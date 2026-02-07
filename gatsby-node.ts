e88defd2db9f6b09048cff7f75bcf4dc7757b464
Jamie McMillan <jamie.mcmillan@satellitevu.com>
2026-02-07 22:02:46 +0000

    fix: explicitly ignore eleventy files

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,3 +1,12 @@
+import type { GatsbyNode } from "gatsby";
+
 export { createPages } from "./internal/gatsby/create-pages";
 export { onCreateNode } from "./internal/gatsby/on-create-node";
 export { onCreateWebpackConfig } from "./internal/gatsby/on-create-webpack-config";
+
+// Skip Eleventy directory data files that Gatsby picks up from src/pages/
+export const onCreatePage: GatsbyNode["onCreatePage"] = ({ page, actions }) => {
+  if (page.path.includes("11tydata")) {
+    actions.deletePage(page);
+  }
+};
